edc2773d7641a67785ded1623085c03ac31d6504
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-21 08:44:51 +0300

    feat: add views and models

--- app.js
+++ app.js
@@ -9,16 +9,34 @@
 // Система маршрутизация как дополнительный компонент сопоставляет запросы с маршрутами и выбирает для обработки запросов определенный контроллер
 
 
+// const express = require("express");
+// const app = express();
+// const userRouter = require("./routes/userRouter");
+// const homeRouter = require("./routes/homeRouter");
+ 
+// app.use("/users", userRouter);;
+// app.use("/", homeRouter);
+ 
+// app.use( (req, res, next)=> {
+//     res.status(404).send("Not Found")
+// });
+ 
+// app.listen(3000);
+
+
 const express = require("express");
 const app = express();
-const userRouter = require("./routes/userRouter");
-const homeRouter = require("./routes/homeRouter");
+const userRouter = require("./routes/userRouter.js");
+const homeRouter = require("./routes/homeRouter.js");
  
-app.use("/users", userRouter);;
+app.set("view engine", "hbs");
+app.use(express.urlencoded({ extended: false }));
+ 
+app.use("/users", userRouter);
 app.use("/", homeRouter);
  
-app.use( (req, res, next)=> {
-    res.status(404).send("Not Found")
+app.use((req, res, next)=> {
+    res.status(404).send("Not Found");
 });
  
 app.listen(3000);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,21 @@
-exports.addUser = (req, res)=>res.send("Добавление пользователя");
+// exports.addUser = (req, res)=>res.send("Добавление пользователя");
 
-exports.getUsers = (req, res)=>res.send("Список пользователей");
+// exports.getUsers = (req, res)=>res.send("Список пользователей");
+
+
+const User = require("../models/user.js");
+ 
+exports.addUser =(req, res)=>res.render("create.hbs");
+
+exports.getUsers = (req, res)=>
+    res.render("users.hbs", {
+        users: User.getAll()
+    });
+
+exports.postUser= (req, res)=>{
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = new User(userName, userAge);
+    user.save();
+    res.redirect("/users");
+};
